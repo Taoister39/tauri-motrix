39e22ddfabcd271305f8ab2f87c069edd149de7c
Taoister39 <Qiacrkeng@gmail.com>
2025-06-21 23:42:59 +0800

    style: menu button

--- apps/website/src/components/MenuButton.tsx
+++ apps/website/src/components/MenuButton.tsx
@@ -1,7 +1,27 @@
-function MenuButton() {
+export interface MenuButtonProps {
+  open?: boolean;
+}
+
+function MenuButton({ open }: MenuButtonProps) {
   return (
-    <button className="bg-transparent border-0 transform before:translate-y-[-10px] before:scale-x-[.8]">
-      <span className="h-0.5 w-full bg-[#5f656f]">Menu</span>
+    <button
+      className={
+        "border-0 h-6 w-7.5 relative cursor-pointer" +
+        " before:scale-x-[.8] before:contents-[''] before:absolute before:top-1/2 before:left-0 before:right-0 before:h-0.5 before:bg-[#5f656f]" +
+        " after:scale-x-[.8] after:contents-[''] after:absolute after:top-1/2 after:left-0 after:right-0 after:h-0.5 after:bg-[#5f656f]" +
+        (open
+          ? " after:-rotate-45 before:rotate-45 after:top-1/2 before:top-1/2"
+          : "")
+      }
+    >
+      <span
+        className={
+          "h-0.5 absolute left-0 right-0 bg-[#5f656f] overflow-hidden" +
+          (open ? " opacity-0" : "")
+        }
+      >
+        Menu
+      </span>
     </button>
   );
 }
