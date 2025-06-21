f804110e37ae0aca602a51403dd4d1bdb5ad794b
Taoister39 <Qiacrkeng@gmail.com>
2025-06-21 23:52:05 +0800

    fix: open is false

--- apps/website/src/components/MenuButton.tsx
+++ apps/website/src/components/MenuButton.tsx
@@ -1,18 +1,20 @@
 export interface MenuButtonProps {
   open?: boolean;
+  onClick?: () => void;
 }
 
-function MenuButton({ open }: MenuButtonProps) {
+function MenuButton({ open, onClick }: MenuButtonProps) {
   return (
     <button
       className={
-        "border-0 h-6 w-7.5 relative cursor-pointer" +
-        " before:scale-x-[.8] before:contents-[''] before:absolute before:top-1/2 before:left-0 before:right-0 before:h-0.5 before:bg-[#5f656f]" +
-        " after:scale-x-[.8] after:contents-[''] after:absolute after:top-1/2 after:left-0 after:right-0 after:h-0.5 after:bg-[#5f656f]" +
+        "border-0 h-6 w-7.5 relative cursor-pointer transition-[rotate_.25s]" +
+        " before:scale-x-[.8] before:contents-[''] before:absolute before:top-0 before:left-0 before:right-0 before:h-0.5 before:bg-[#5f656f]" +
+        " after:scale-x-[.8] after:contents-[''] after:absolute after:bottom-0 after:left-0 after:right-0 after:h-0.5 after:bg-[#5f656f]" +
         (open
           ? " after:-rotate-45 before:rotate-45 after:top-1/2 before:top-1/2"
           : "")
       }
+      onClick={onClick}
     >
       <span
         className={
