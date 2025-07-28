22872829e43ef2cfd83781e01606f86322f12214
Taoister39 <Qiacrkeng@gmail.com>
2025-07-28 18:09:41 +0800

    feat: tracker autocomplete

--- src/business/setting/BtTrackerDialog.tsx
+++ src/business/setting/BtTrackerDialog.tsx
@@ -1,3 +1,4 @@
+import CheckIcon from "@mui/icons-material/Check";
 import {
   Autocomplete,
   Box,
@@ -8,11 +9,13 @@ import {
   useTheme,
 } from "@mui/material";
 import { useBoolean } from "ahooks";
-import { Ref, useImperativeHandle } from "react";
+import { Ref, useImperativeHandle, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import MonacoEditor from "react-monaco-editor";
 
 import BaseDialog, { DialogRef } from "@/components/BaseDialog";
+import Tag from "@/components/Tag";
+import { TRACKER_SOURCE_OPTIONS } from "@/constant/speed";
 
 const TheQuick = styled(Box)`
   position: absolute;
@@ -33,6 +36,19 @@ function BtTrackerDialog(props: { ref: Ref<DialogRef> }) {
     close: setFalse,
   }));
 
+  const trackerOptions = useMemo(() => {
+    return TRACKER_SOURCE_OPTIONS.flatMap((x) => {
+      const group = x.label;
+
+      return x.options.map((y) => {
+        const title = y.label;
+        const url = y.value;
+        const cdn = y.cdn;
+        return { group, title, url, cdn };
+      });
+    });
+  }, []);
+
   return (
     <BaseDialog
       open={open}
@@ -51,7 +67,39 @@ function BtTrackerDialog(props: { ref: Ref<DialogRef> }) {
         pt: "6px !important",
       })}
     >
-      <Grouped />
+      <Autocomplete
+        multiple
+        size="small"
+        options={trackerOptions}
+        groupBy={(option) => option.group}
+        getOptionLabel={(option) => option.title}
+        fullWidth
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Tracker Servers Autocomplete"
+            variant="standard"
+          />
+        )}
+        renderOption={(props, option, { selected }) => {
+          const { key, ...optionProps } = props;
+          return (
+            <li key={key} {...optionProps}>
+              <span style={{ flexGrow: 1 }}>{option.title}</span>
+              {option.cdn && (
+                <Tag type="success" style={{ marginRight: 24 }}>
+                  CDN
+                </Tag>
+              )}
+              <CheckIcon
+                fontSize="small"
+                style={{ visibility: selected ? "visible" : "hidden" }}
+              />
+            </li>
+          );
+        }}
+        disableCloseOnSelect
+      />
       <MonacoEditor
         language="txt"
         theme={themeMode === "light" ? "vs" : "vs-dark"}
@@ -66,62 +114,4 @@ function BtTrackerDialog(props: { ref: Ref<DialogRef> }) {
   );
 }
 
-function Grouped() {
-  const options = top100Films.map((option) => {
-    const firstLetter = option.title[0].toUpperCase();
-    return {
-      firstLetter: /[0-9]/.test(firstLetter) ? "0-9" : firstLetter,
-      ...option,
-    };
-  });
-
-  return (
-    <Autocomplete
-      size="small"
-      multiple
-      options={options.sort(
-        (a, b) => -b.firstLetter.localeCompare(a.firstLetter),
-      )}
-      groupBy={(option) => option.firstLetter}
-      getOptionLabel={(option) => option.title}
-      fullWidth
-      renderInput={(params) => (
-        <TextField {...params} label="With categories" variant="standard" />
-      )}
-    />
-  );
-}
-
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const top100Films = [
-  { title: "The Shawshank Redemption", year: 1994 },
-  { title: "The Godfather", year: 1972 },
-  { title: "The Godfather: Part II", year: 1974 },
-  { title: "The Dark Knight", year: 2008 },
-  { title: "12 Angry Men", year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: "Pulp Fiction", year: 1994 },
-  {
-    title: "The Lord of the Rings: The Return of the King",
-    year: 2003,
-  },
-  { title: "The Good, the Bad and the Ugly", year: 1966 },
-  { title: "Fight Club", year: 1999 },
-  {
-    title: "The Lord of the Rings: The Fellowship of the Ring",
-    year: 2001,
-  },
-  {
-    title: "Star Wars: Episode V - The Empire Strikes Back",
-    year: 1980,
-  },
-  { title: "Forrest Gump", year: 1994 },
-  { title: "Inception", year: 2010 },
-  {
-    title: "The Lord of the Rings: The Two Towers",
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: "Goodfellas", year: 1990 },
-];
 export default BtTrackerDialog;
